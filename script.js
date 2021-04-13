4beda9e192673eb3e44de629ff7150ca7219fb17
unknown <rsfbernardes@hotmail.com>
2021-04-12 21:56:49 -0300

    create function startGame

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let canvas = document.getElementById("crazy-snake-game");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let direction = "right";
+let game = setInterval(startGame, 100);
 
 snake[0] = {
     x: 8 * box,
@@ -20,5 +22,24 @@ function createSnake() {
     }
 }
 
-createBG();
-createSnake();
+function startGame() {
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
