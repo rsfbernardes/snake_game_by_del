760568912ffe89029c749ce13fe6bd9f2fbcd04a
unknown <rsfbernardes@hotmail.com>
2021-04-14 20:41:13 -0300

    fixed bug snake desapear when canvas has more then 512px, e.g. 16 * box

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 let direction = "right";
-let game = setInterval(startGame, 1000);
+let game = setInterval(startGame, 100);
+let score = 0;
 
 snake[0] = {
-    x: 0 * box,
-    y: 0 * box
+    x: 8 * box,
+    y: 8 * box
 }
 
 let food = {
@@ -35,13 +36,21 @@ function createFood(){
 document.addEventListener('keydown', update);
 
 function update(event) {
-    if(event.keyCode == 37 && direction != "right") direction = "left";
-    if(event.keyCode == 38 && direction != "down") direction = "up";
-    if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down";
+    if(snake[0].x >= 0 && snake[0].x <= 15 * box && snake[0].y >= 0 && snake[0].y <= 15 * box) {
+        if(event.keyCode == 37 && direction != "right") direction = "left";
+        if(event.keyCode == 38 && direction != "down") direction = "up";
+        if(event.keyCode == 39 && direction != "left") direction = "right";
+        if(event.keyCode == 40 && direction != "up") direction = "down";
+    }
 }
 
 function startGame() {
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 15 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 15 * box;
+    
     createBG();
     createSnake();
     createFood();
@@ -52,11 +61,6 @@ function startGame() {
             alert('G4M3 0V3R! O Mito sentou na cobra');
         }
     }
-
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
